0326fcabe2258cb754ee84b1b71d782a907506e7
beksultan-berik <beslatnberik@gmail.com>
2024-09-28 20:52:56 +0500

    Initial Commit

---
+++ Madina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Madina
+//
+//  Created by Smart Castle M1A2002 on 28.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
